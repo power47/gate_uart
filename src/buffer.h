cc1afa1a39f3c211280b373d0e1c5512a84984d9
Alexey <alexey@localhost.localdomain>
2021-11-15 16:06:27 +0300

    Move API for UART to separate repo

--- src/buffer.h
+++ src/buffer.h
@@ -14,5 +14,3 @@ BUFFER_EXTLBL int buffer_push (uint8_t);
 BUFFER_EXTLBL uint8_t 	buffer [256];
 BUFFER_EXTLBL int 		buffer_len;
 BUFFER_EXTLBL int 		buffer_ind;
-
-
--- src/main.c
+++ src/main.c
@@ -4,14 +4,14 @@
 
 #include "init.h"
 #include "buffer.h"
-#include "uart.h"
+#include "api_uart.h"
 
 
-UART_t UART_1;
-UART_t UART_2;
+API_UART_t API_UART_i;
+API_UART_t API_UART_o;
 
-void UART_1_callback_Rx (uint32_t data);
-void UART_2_callback_Tx (uint32_t data);
+void API_UART_i_callback_Rx (uint32_t data);
+void API_UART_o_callback_Tx (uint32_t data);
 
 
 
@@ -21,11 +21,11 @@ int main (void)
 
 	buffer_Init ();
 
-	UART_Init (&UART_1, UART1, 84000000,   9600, UART_PARITY_EVEN, UART_STOP_1);
-	UART_Init (&UART_2, UART2, 42000000, 115200, UART_PARITY_EVEN, UART_STOP_1);
-	
-	UART_set_callback_Rx (&UART_1, UART_1_callback_Rx);
-	UART_set_callback_Tx (&UART_2, UART_2_callback_Tx);
+	API_UART_Init (&API_UART_i, API_UART1, 84000000,   9600, API_UART_PARITY_EVEN, API_UART_STOP_1);
+	API_UART_Init (&API_UART_o, API_UART2, 42000000, 115200, API_UART_PARITY_EVEN, API_UART_STOP_1);
+
+	API_UART_set_callback_Rx (&API_UART_i, API_UART_i_callback_Rx);
+	API_UART_set_callback_Tx (&API_UART_o, API_UART_o_callback_Tx);
 
 	NVIC_SetPriority ( 
 		USART1_IRQn, 
@@ -50,7 +50,7 @@ int main (void)
 }
 
 
-void UART_1_callback_Rx (uint32_t data)
+void API_UART_i_callback_Rx (uint32_t data)
 {
 	int i = -1;
 
@@ -59,12 +59,12 @@ void UART_1_callback_Rx (uint32_t data)
 
 	if (i >= 0)
 	{
-		UART_transmit (&UART_2, buffer + i, 128);
+		API_UART_transmit (&API_UART_o, buffer + i, 128);
 	}
 }
 
 
-void UART_2_callback_Tx (uint32_t data)
+void API_UART_o_callback_Tx (uint32_t data)
 {
 }
 
@@ -75,11 +75,11 @@ void UART_2_callback_Tx (uint32_t data)
 //
 void USART1_IRQHandler (void)
 {
-	UART_IRQ_Handler (&UART_1);
+	API_UART_IRQ_Handler (&API_UART_i);
 }
 
 void USART2_IRQHandler (void)
 {
-	UART_IRQ_Handler (&UART_2);
+	API_UART_IRQ_Handler (&API_UART_o);
 }
 
